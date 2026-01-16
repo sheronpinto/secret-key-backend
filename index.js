b4a023c534afeaaddf46f581e6424e169320a363
Sheron Pinto <sheronpinto@gmail.com>
2026-01-16 23:25:31 +0530

    Enable CORS for frontend

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { sendSecretKey } from "./emailService.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 function generateSecretKey() {
